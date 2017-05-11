610d03dbaaa96df8ab2c484fb8a41436e47cb5b5
gloriouseggroll <gloriouseggroll@gmail.com>
2017-05-11 12:30:51 -0400

    update license header

--- src/me/gloriouseggroll/quorrabot/event/musicplayer/MusicPlayerConnectEvent.java
+++ src/me/gloriouseggroll/quorrabot/event/musicplayer/MusicPlayerConnectEvent.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2016 www.quorrabot.com
+ * Copyright (C) 2017 www.quorrabot.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
